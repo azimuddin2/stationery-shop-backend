3c73a83fdd7ffe4c80706e87213e2891c583c3dc
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-11-23 22:30:44 +0600

    product interface and model

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,51 @@
+import { Schema, model, connect } from 'mongoose';
+import { Product, ProductCategory } from './product.interface';
+
+const productSchema = new Schema<Product>({
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        trim: true,
+        minlength: [3, 'The length of product name can be minimum 3 characters'],
+        maxlength: [80, 'The length of product name can be maximum 80 characters '],
+    },
+    brand: {
+        type: String,
+        required: [true, 'Product brand name is required'],
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price is required"],
+        min: [0.01, "Price must be a positive number"],
+        validate: {
+            validator: (v: number) => v > 0,
+            message: (props: { value: number }) => `${props.value} is not a valid price`,
+        },
+    },
+    category: {
+        type: String,
+        required: [true, "Category is required"],
+        enum: Object.values(ProductCategory),
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+        trim: true,
+        maxlength: [500, "Description cannot exceed 500 characters"],
+    },
+    quantity: {
+        type: Number,
+        required: [true, "Quantity is required"],
+        min: [0, "Quantity cannot be negative"],
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+        default: true,
+    },
+}, { timestamps: true });
+
+const ProductModel = model<Product>('Product', productSchema);
+
+export default ProductModel;
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import app from './app'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    await mongoose.connect(config.database_url as string);
+    console.log('Connection to database is successfully established');
 
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(error)
+    console.log('Could not connect to DB', error);
   }
 }
 
